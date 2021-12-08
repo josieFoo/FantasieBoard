7440e9cebca8d0d472bdddbea0b71e3e4d95d90b
josiepark <josiepark@mi.fu-berlin.de>
2021-12-08 20:20:39 +0100

    model erstellen

--- fantasieboard/boards/admin.py
+++ fantasieboard/boards/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Community
+from .models import Community, Users
 
 admin.site.register(Community)
+admin.site.register(Users)
--- fantasieboard/boards/models.py
+++ fantasieboard/boards/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class Community():
-	#id
-	community_name = models.TextField()
+class Community(models.Model):
+	id = models.AutoField(primary_key=True)
+	community_name = models.CharField(blank=False, max_length=32)
 
-#class Community_moderator():
-#	...
-#class Users():
-#	...
-#class Articles():
-#	...
-#class Comments():
+class Users(models.Model):
+	user_id = models.AutoField(primary_key=True)
+	superuser = models.BooleanField(null=False, default=False)
+	pseudo_name = models.CharField(blank=False, max_length=16)
+	created_at = models.DateField(auto_now_add=True)
+	mail_address = models.EmailField(max_length=254, blank=False)
+	password = models.CharField(blank=False, max_length=16)
+ 
+#class Community_moderator(models.Model):
+#	community_id = models.ForeignKey(Community, on_delete=models.CASCADE)
+#	admin_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+#	
+#class Articles(models.Model):
+#	community_id = models.Foreignkey(Community, on_delete=models.CASCADE)
+#	article_num = models.AutoField(primary_key=True)
+#	title = models.CharField(blank=False, max_length=128)
+#	author_id = models.Foreignkey(Users, on_delete=models.CASCADE)
+#class Comments(models.Model):
 #	...
 #class Likes():
 #	...
-# 
