9f6b264c9e16e49d39fc00b06472bbccf2b8c419
josiepark <josiepark@mi.fu-berlin.de>
2021-12-08 14:54:26 +0100

    test

--- fantasieboard/boards/models.py
+++ fantasieboard/boards/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Community():
+	#id
+	community_name = models.TextField()
+
+#class Community_moderator():
+#	...
+#class Users():
+#	...
+#class Articles():
+#	...
+#class Comments():
+#	...
+#class Likes():
+#	...
+# 
