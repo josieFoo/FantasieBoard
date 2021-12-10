fb6a0da01733874379c57dbee2c25b752ff75be0
josiepark <josiepark@mi.fu-berlin.de>
2021-12-10 18:47:01 +0100

    model, gitignore modified

--- fantasieboard/boards/models.py
+++ fantasieboard/boards/models.py
@@ -21,7 +21,8 @@ class Community_moderator(models.Model):
  
 class Articles(models.Model):
 	community_id = models.ForeignKey("Community", on_delete=models.CASCADE)
-	article_num = models.AutoField(primary_key=True)
+	#article_id = models.AutoField(primary_key=True) -> .pk
+	article_num = models.IntegerField(unique=True)
 	title = models.CharField(blank=False, max_length=128)
 	author_id = models.ForeignKey("Users", on_delete=models.CASCADE)
 	pinned = models.BooleanField(null=False, default=False)
@@ -29,14 +30,29 @@ class Articles(models.Model):
 	rich_txt = models.TextField(max_length=400, blank=False, default=" ")
  
 class Comments(models.Model):
-	community_id = models.ForeignKey("Community", on_delete=models.CASCADE)
-	article_num = models.ForeignKey("Articles", on_delete=models.CASCADE)
+	#community_id = models.ForeignKey("Community", on_delete=models.CASCADE)
 	#comment_id = models.BigAutoField(unique=True) -> Comments.pk
-	user_id = models.ForeignKey("Users.pseudo_name", on_delete=models.CASCADE)
+	article_id = models.ForeignKey("Articles", on_delete=models.CASCADE)
+	user_id = models.ForeignKey("Users", on_delete=models.CASCADE)
 	written_on = models.DateField(auto_now=True)
 	rich_txt = models.TextField(max_length=100, blank=False, default=" ")
  
 class Likes(models.Model):
-	community_id = models.ForeignKey("Community", on_delete=models.CASCADE)
-	article_num = models.ForeignKey("Articles", on_delete=models.CASCADE)
+	#community_id = models.ForeignKey("Community", on_delete=models.CASCADE)
+	#article_num = models.ForeignKey("Articles", on_delete=models.CASCADE)
+	article_id = models.ForeignKey("Articles", on_delete=models.CASCADE)
 	user_id = models.ForeignKey("Users", on_delete=models.CASCADE)
+
+"""
+ class Comment(models.Model):
+	article = models.ForeignKey("Articles")
+
+omment_obj = Comment()
+article_obj = Article()
+
+comment_obj.article = article_obj
+comment_obj.article_id = article_obj.pk
+# Beides geht!
+ 
+ """
+ 
