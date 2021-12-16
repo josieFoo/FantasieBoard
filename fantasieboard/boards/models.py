7b9ecc13aa2fb617299bc721f4fe13ad3d2f399d
josiepark <josiepark@mi.fu-berlin.de>
2021-12-16 15:10:41 +0100

    article_num deleted

--- fantasieboard/boards/models.py
+++ fantasieboard/boards/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Community(models.Model):
 	"""
 	Diese Klasse beinhaltet die Tabelle der Namen von Communities.
-	Keine redundanten Namen erlaubt.
+	Keine redundanten Namen erlaubt. (.pk) implizit erstellt.
 	"""
 
 	community_name = models.CharField(blank=False, max_length=32, unique=True)
@@ -39,17 +39,18 @@ class Community_moderator(models.Model):
 	admin_id = models.ForeignKey("Users", on_delete=models.CASCADE)
 	
 	def __str__(self) -> str:
-		return str(self.community_id)
+		return f"{str(self.admin_id)}, {str(self.community_id)}"
  
 class Articles(models.Model):
 	"""
 	Beinhaltet die Metadaten über die Beiträge sowie wer, wann, wo geschrieben hat etc.
 	Jeder Beitrag hat 'community_id' und 'article_num' und somit primary key gebildet.
 	Wenn 'pinned=True' dann, wird der Beitrag auf der obererste Zeile gepinnt.
+	TODO: Entweder 'community_id' + 'article_num' als pk oder 
+ 	'global_article_id'als pk soll her.
 	"""
 	
 	community_id = models.ForeignKey("Community", on_delete=models.CASCADE)
-	article_num = models.IntegerField(unique=True)
 	title = models.CharField(blank=False, max_length=128)
 	author_id = models.ForeignKey("Users", on_delete=models.CASCADE)
 	pinned = models.BooleanField(null=False, default=False)
@@ -57,15 +58,22 @@ class Articles(models.Model):
 	rich_txt = models.TextField(max_length=400, blank=False, default=" ")
 	
 	def __str__(self) -> str:
-		return str(self.community_id) + str(self.article_num)
+		return str(self.community_id) + str(self.pk)
 
 class Comments(models.Model):
-	#community_id = models.ForeignKey("Community", on_delete=models.CASCADE)
-	#comment_id = models.BigAutoField(unique=True) -> Comments.pk
+	"""
+	Model für Kommentare.
+	Ein Kommentar hat einen Autor und den darauf beziehenden Beitrag.
+	"""
+	
 	article_id = models.ForeignKey("Articles", on_delete=models.CASCADE)
 	user_id = models.ForeignKey("Users", on_delete=models.CASCADE)
 	written_on = models.DateField(auto_now=True)
 	rich_txt = models.TextField(max_length=100, blank=False, default=" ")
+	
+	def __str__(self):
+		return str(self.pk) + str(self.article_id)
+
  
 class Likes(models.Model):
 	#community_id = models.ForeignKey("Community", on_delete=models.CASCADE)
