e3e5ccfc3f232f8be953f5cdbba7f7c8f0221b5a
josiepark <josiepark@mi.fu-berlin.de>
2021-12-17 20:46:54 +0100

    class method updated

--- fantasieboard/boards/models.py
+++ fantasieboard/boards/models.py
@@ -1,18 +1,6 @@
 """ Model Definitionen für Boards """
+from __future__ import annotations
 from django.db import models
-
-
-class Community(models.Model):
-	"""
-	Diese Klasse beinhaltet die Tabelle der Namen von Communities.
-	Keine redundanten Namen erlaubt. (.pk) implizit erstellt.
-	"""
-
-	community_name = models.CharField(blank=False, max_length=32, unique=True)
-	
-	def __str__(self):
-		return str(self.community_name)
-
 
 class Users(models.Model):
 	"""
@@ -30,6 +18,44 @@ class Users(models.Model):
 	def __str__(self):
 		return str(self.pseudo_name)
 
+	def createCommunity(self, name: str, moderators: list):
+		"""
+		0. ist der User superuser?
+		1. sollte überprüft werden, ob eine Community mit dem gleichen Namen existiert
+		2. wer die Moderatoren sind.
+  		"""
+		if self.superuser:
+			if not Community.objects.filter(community_name = name).exists():
+				new_community = Community.objects.create(community_name = name)
+				for mod in moderators:
+					new_community.add_moderator(mod)
+				return new_community
+		return None
+
+
+class Community(models.Model):
+	"""
+	Diese Klasse beinhaltet die Tabelle der Namen von Communities.
+	Keine redundanten Namen erlaubt. (.pk) implizit erstellt.
+	"""
+
+	community_name = models.CharField(blank=False, max_length=32, unique=True)	
+ 
+	def __str__(self):
+		return str(self.community_name)
+	
+	def add_moderator(self, user: Users):
+		return Community_moderator.objects.create(
+               community_id = self, admin_id = user)
+
+	
+class CommunityManager():
+	...
+ 
+ 
+class UsersManager():
+	...
+ 
 
 class Community_moderator(models.Model):
 	"""
@@ -44,7 +70,16 @@ class Community_moderator(models.Model):
 	def __str__(self) -> str:
 		return f"{str(self.admin_id)}_{str(self.community_id)}"
 
+	class Meta:
+		"""
+		Verhindert die Redundanz
+		"""
+		unique_together = (('community_id', 'admin_id'),)
 
+class ModeratorManager():
+	...
+
+ 
 class Articles(models.Model):
 	"""
 	Beinhaltet die Daten über die Beiträge sowie wer, wann, wo geschrieben hat etc.
@@ -60,7 +95,7 @@ class Articles(models.Model):
 	rich_txt = models.TextField(max_length=400, blank=False, default=" ")
 	
 	def __str__(self) -> str:
-		return f"{str(self.pk)}_{str(self.community_id)}"
+		return f"{str(self.pk)}_{str(self.community_id)}_{str(self.title)}_{str(self.author_id)}"
 
 
 class Comments(models.Model):
@@ -80,16 +115,23 @@ class Comments(models.Model):
 
 class Likes(models.Model):
 	"""
- 	Model für Likes-Zähler.
+	Model für Likes-Zähler.
 	TODO: Je ein User für je ein Article ein Like erlaubt. 
- 		  Redundante Likes verhindern.
-  	"""
+		  Redundante Likes verhindern.
+	"""
 
 	article_id = models.ForeignKey("Articles", on_delete=models.CASCADE)
 	user_id = models.ForeignKey("Users", on_delete=models.CASCADE)
  
-	def __str__(self):
-		return f"{str(self.article_id)}_{str(self.user_id)}"
+	class Meta:
+		unique_together = (('article_id', 'user_id'),)
 
+	def __str__(self):
+		return f"{str(self.user_id)} likes {str(self.article_id)}"
+
+
+class LikesManager():
+	...
+ 
 
 """Ende"""
