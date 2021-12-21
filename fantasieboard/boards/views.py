54878bf598d2e4f4f53d3b3b86d958dea97dae32
josiepark <josiepark@mi.fu-berlin.de>
2021-12-21 12:50:49 +0100

    home view updated

--- fantasieboard/boards/views.py
+++ fantasieboard/boards/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_view(request):
+    return HttpResponse("<h1> Welcome Home, Heroes!</h1>")
