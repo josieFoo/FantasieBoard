de05a7ef658ddf17f48bb729240a9825566077c0
josiepark <josiepark@mi.fu-berlin.de>
2022-03-02 04:32:24 +0100

    admin frontend update

--- fantasieboard/boards/admin.py
+++ fantasieboard/boards/admin.py
@@ -3,9 +3,23 @@ from django.contrib import admin
 # Register your models here.
 from .models import Community, Community_moderator, Articles, Comments, Likes
 
+class ArticlesAdmin(admin.ModelAdmin):
+	list_display = ('author_id', 'community_id', 'title', 'written_on')
+	list_filter = ('pinned',)
+	search_fields = ['author_id', 'community_id', 'title']
+ 
+class CommunityAdmin(admin.ModelAdmin):
+	list_display = ('community_name',)
+	list_filter = ()
+	search_fields = ['community_name']
+ 
+class CommentsAdmin(admin.ModelAdmin):
+	list_display = ('article_id', 'user_id', 'written_on')
+	list_filter = ()
+	search_fields = ['article_id']
 
-admin.site.register(Community)
+admin.site.register(Community, CommunityAdmin)
 admin.site.register(Community_moderator)
-admin.site.register(Articles)
-admin.site.register(Comments)
+admin.site.register(Articles, ArticlesAdmin)
+admin.site.register(Comments, CommentsAdmin)
 admin.site.register(Likes)
